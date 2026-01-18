f4b2f0aebb7a6eb4ad947c268b8df67adfe7ef1f
Dmitriy-495 <tda495@yandex.ru>
2026-01-18 17:24:05 +0300

    .VENV

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Static
+import os
+import asyncio
+
+from dotenv import load_dotenv
+import websockets
+
+from textual.app import App
+from textual.widgets import RichLog
+
+
+load_dotenv()
+
+WS_URL = os.getenv("WS_URL")
+WS_AUTH_TOKEN = os.getenv("WS_AUTH_TOKEN")
 
 
 class BotApp(App):
-    def compose(self) -> ComposeResult:
-        yield Static("I'm BOT")
+    async def on_mount(self) -> None:
+        self.ws_log = RichLog()
+        await self.mount(self.ws_log)
+        self.ws_log.write("CONNECTING...")
+        asyncio.create_task(self.ws_loop())
+
+    async def ws_loop(self):
+        headers = {
+            "Authorization": f"Bearer {WS_AUTH_TOKEN}"
+        }
+
+        try:
+            async with websockets.connect(WS_URL, headers=headers) as ws:
+                self.ws_log.write(f"CONNECTED: {WS_URL}")
+                async for msg in ws:
+                    self.ws_log.write(msg)
+        except Exception as e:
+            self.ws_log.write(f"WS ERROR: {e}")
 
 
 if __name__ == "__main__":
